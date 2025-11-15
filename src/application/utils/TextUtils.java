9efad43868f40168456622a0b95de258367404b3
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-15 17:23:59 -0500

    docs: add comments on text utils

--- src/application/utils/TextUtils.java
+++ src/application/utils/TextUtils.java
@@ -8,6 +8,11 @@ import java.io.IOException;
 
 public record TextUtils() {
 
+    /**
+     * statically get a file content as String line.
+     * @param fileURI - the file to read its content.
+     * @return the file content.
+     */
     public static String getFileLines(String fileURI) {
         StringBuilder lines = new StringBuilder();
         try(BufferedReader buffer = new BufferedReader(new FileReader(new File(fileURI)))) {
@@ -21,6 +26,12 @@ public record TextUtils() {
         }
         return lines.toString();
     }
+    /**
+     * statically write a file with the provided lines.
+     * @param fileURI - the file to write the lines.
+     * @param lines - the lines to write.
+     * @param append - if you want to re-write or append to the existent content.
+     */
     public static void writeLines(String fileURI, String lines, boolean append) {
         if(lines.isBlank()) return; 
         try(FileWriter writer = new FileWriter(new File(fileURI), append)) {
