0de42be58437021f0093f095f610696bd32134d6
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-15 13:32:01 -0500

    feat: add create dirs and list dirs content

---
+++ src/application/utils/FileUtils.java
@@ -0,0 +1,39 @@
+package application.utils;
+
+import java.util.List;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.FileVisitOption;
+
+
+public class FileUtils {
+
+    public boolean create(String pathURI) {
+        File f = new File(pathURI);
+        if(f.isFile()) {
+            System.out.println("[Info] Only directories allowed");
+            return false;
+        }
+        if(f.toPath().getNameCount() > 2) {
+            return f.mkdirs();
+        }
+        return f.mkdir();
+
+    }
+
+    public List<Path> listDirContent(String pathURI, int level) {
+        if (!(level > 0)) level = Integer.MAX_VALUE;
+        try {
+            return Files.walk(Paths.get(pathURI), level, FileVisitOption.FOLLOW_LINKS).toList();
+        } catch(IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
