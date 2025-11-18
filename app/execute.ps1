1f5746a718d77cdbe1934637230e84b02669d0f8
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-18 18:10:06 -0500

    build: update execute script

--- app/execute.ps1
+++ app/execute.ps1
@@ -1,3 +1,3 @@
 $AssignEntry = "jar -ufe app\build\libs\app.jar org.example.App"
-$Command = "java -jar app\build\libs\app.jar --cp app\README.md app\execute.ps1 To app\docs app\lib"
+$Command = "java -jar app\build\libs\app.jar"
 Invoke-Expression ($AssignEntry + " && " + $Command )
