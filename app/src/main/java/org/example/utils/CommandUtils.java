217572a1b0449a3f8608c24228299c14941c45f4
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-24 21:33:05 -0500

    refactor: better indentation on help messages

--- app/src/main/java/org/example/utils/CommandUtils.java
+++ app/src/main/java/org/example/utils/CommandUtils.java
@@ -29,11 +29,11 @@ public class CommandUtils {
         help.append("Use --ni new.txt to create a new empty file.");
         help.append("\n\t");
         help.append("If you want to create multiple files at the same time");
-        help.append("\n");
+        help.append("\n\t");
         help.append(" => Use --ni new.txt other.txt to create the files.");
         help.append("\n\t");
         help.append("If you want to create a file in a nested structure");
-        help.append("\n");
+        help.append("\n\t");
         help.append(" => Use --ni custom");
         help.append(File.separator);
         help.append("new.txt to create the file in the nested structure.");
@@ -51,11 +51,11 @@ public class CommandUtils {
         help.append("Use --md new-path to create a new empty directory.");
         help.append("\n\t");
         help.append("If you want to create multiple directories at the same time");
-        help.append("\n");
+        help.append("\n\t");
         help.append(" => Use --md new-path other-path to create the directories.");
         help.append("\n\t");
         help.append("If you want to create a directory in a nested structure");
-        help.append("\n");
+        help.append("\n\t");
         help.append(" => Use --md custom");
         help.append(File.separator);
         help.append("new-path to create the directory in the nested structure.");
